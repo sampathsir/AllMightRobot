a8429bc07d38bc1b93ea1dd341a6131db32c960f
DragSama <49992845+DragSama@users.noreply.github.com>
2020-12-10 18:12:53 +0530

    Remove useless code and add missing return

--- AllMightRobot/modules/pm_menu.py
+++ AllMightRobot/modules/pm_menu.py
@@ -97,11 +97,12 @@ async def help_cmd(message, strings):
 
 @register(helpmenu_cb.filter(), f='cb', allow_kwargs=True)
 async def helpmenu_callback(query, callback_data=None, **kwargs):
-    helpmenu_cb
     mod = callback_data['mod']
     if not mod in MOD_HELP:
         await query.answer()
+        return
     msg = f"Help for *{mod}*\n\n"
     msg += f"{MOD_HELP[mod]}"
     with suppress(MessageNotModified):
         await query.message.edit_text(msg)
+        await query.answer('Help for ' + mod)
